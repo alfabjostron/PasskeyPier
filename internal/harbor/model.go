9a93624b88c07bff1cb5971e6e284902d088fea3
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2016-12-04 15:34:00 +0700

    docs: document relying party ids

--- internal/harbor/model.go
+++ internal/harbor/model.go
@@ -8,3 +8,11 @@ import (
 	"fmt"
 )
 
+// CeremonyType distinguishes the two WebAuthn ceremonies.
+type CeremonyType string
+
+const (
+	// TypeCreate is the value of clientDataJSON.type during registration.
+	TypeCreate CeremonyType = "webauthn.create"
+	// TypeGet is the value of clientDataJSON.type during authentication.
+	TypeGet CeremonyType = "webauthn.get"
