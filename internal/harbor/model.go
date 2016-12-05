b0130e193fc1b5f91d9d7a9f21e5d0dcf08ec0b2
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2016-12-05 14:15:00 +0700

    fix: correct harbor model

--- internal/harbor/model.go
+++ internal/harbor/model.go
@@ -16,3 +16,12 @@ const (
 	TypeCreate CeremonyType = "webauthn.create"
 	// TypeGet is the value of clientDataJSON.type during authentication.
 	TypeGet CeremonyType = "webauthn.get"
+)
+
+// Authenticator data flag bits (WebAuthn L2 sec. 6.1).
+const (
+	FlagUserPresent    byte = 1 << 0 // UP
+	FlagUserVerified   byte = 1 << 2 // UV
+	FlagBackupEligible byte = 1 << 3 // BE
+	FlagBackupState    byte = 1 << 4 // BS
+	FlagAttestedData   byte = 1 << 6 // AT
