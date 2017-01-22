2cdeeaf514e602d98848ec03c6c36bdd5cb3832d
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-01-22 09:27:00 +0700

    fix: correct ci workflow

--- internal/harbor/challenge.go
+++ internal/harbor/challenge.go
@@ -4,3 +4,7 @@ import (
 	"crypto/rand"
 	"fmt"
 )
+
+// minChallengeLen is the minimum challenge length recommended by the WebAuthn
+// specification (16 bytes). The default used by the lab is 32 bytes.
+const (
