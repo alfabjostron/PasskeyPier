d4c6d2a83fc82730098f5adccca2a37823e2e866
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-01-22 09:39:00 +0700

    docs: document credential registry

--- internal/harbor/challenge.go
+++ internal/harbor/challenge.go
@@ -8,3 +8,6 @@ import (
 // minChallengeLen is the minimum challenge length recommended by the WebAuthn
 // specification (16 bytes). The default used by the lab is 32 bytes.
 const (
+	minChallengeLen     = 16
+	DefaultChallengeLen = 32
+)
