3e3f061c17007fd61cd01bd4c5fad1cc406e6a17
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-02-07 14:25:00 +0700

    refactor: tidy relying party ids

--- internal/harbor/authenticator.go
+++ internal/harbor/authenticator.go
@@ -9,3 +9,11 @@ import (
 
 // COSE algorithm identifier for EdDSA over Ed25519 (RFC 8152 / RFC 9053).
 const COSEAlgEdDSA = -8
+
+// VirtualAuthenticator models a roaming or platform authenticator holding one
+// or more discoverable credentials. It signs assertions with Ed25519 and
+// maintains a per-credential signature counter.
+type VirtualAuthenticator struct {
+	// AAGUID identifies the authenticator model (16 bytes). Zero for privacy.
+	AAGUID [16]byte
+	// SupportsUV indicates the authenticator can perform user verification
