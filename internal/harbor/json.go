59476d3050b5f0037e6a3367e8b20d40fb9007bd
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-02-13 09:37:00 +0700

    style: clean up conformance report

--- internal/harbor/json.go
+++ internal/harbor/json.go
@@ -6,3 +6,6 @@ import (
 )
 
 // decodeStrict decodes JSON, rejecting unknown fields to catch malformed or
+// tampered client data during verification.
+func decodeStrict(data []byte, v any) error {
+	dec := json.NewDecoder(bytes.NewReader(data))
