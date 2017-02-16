7c10b5680f69dfa1f58380dcc42ecc37b562b9dd
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-02-16 16:07:00 +0700

    fix: correct conformance report

--- internal/harbor/json.go
+++ internal/harbor/json.go
@@ -9,3 +9,6 @@ import (
 // tampered client data during verification.
 func decodeStrict(data []byte, v any) error {
 	dec := json.NewDecoder(bytes.NewReader(data))
+	dec.DisallowUnknownFields()
+	return dec.Decode(v)
+}
