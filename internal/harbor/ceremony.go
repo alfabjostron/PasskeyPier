c2ee2c550726be45678e03316e99c975ca143501
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-03-28 10:00:00 +0700

    feat: credential registry

--- internal/harbor/ceremony.go
+++ internal/harbor/ceremony.go
@@ -26,3 +26,18 @@ type AuthenticationResult struct {
 	Signature         []byte
 	UserVerified      bool
 	UserHandle        []byte
+}
+
+// clientOrigin is the origin the virtual client reports. In a browser this is
+// enforced by the user agent; here the client faithfully reports the RP origin
+// unless a scenario deliberately overrides it.
+type clientOrigin struct {
+	origin      string
+	crossOrigin bool
+}
+
+// Register performs a full create ceremony against the RP:
+//
+//	1. the RP issues options with a fresh challenge;
+//	2. the client builds client data (webauthn.create);
+//	3. the authenticator mints an Ed25519 credential and authenticator data;
