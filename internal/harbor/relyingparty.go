f43debd3e09d22058b05b14cb4f75f8dcdc686a2
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-06-08 09:58:00 +0700

    refactor: tidy cli flags

--- internal/harbor/relyingparty.go
+++ internal/harbor/relyingparty.go
@@ -29,3 +29,9 @@ type RelyingParty struct {
 	Origin string // e.g. "https://harbor.example"
 	// Store maps base64url credential id -> registered public-key record.
 	Store map[string]*RegisteredCredential
+}
+
+// RegisteredCredential is the RP's server-side view of a registered passkey.
+type RegisteredCredential struct {
+	ID            []byte
+	PublicKey     []byte // raw Ed25519 public key (32 bytes)
