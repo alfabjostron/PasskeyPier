7d65f656706b4ed2a0449123edb220e446957217
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-07-08 17:44:00 +0700

    refactor: tidy base64url codec

--- internal/harbor/helpers.go
+++ internal/harbor/helpers.go
@@ -11,3 +11,9 @@ func nowNS() int64 { return time.Now().UnixNano() }
 
 // authenticateWithChallengeMismatch signs an assertion over signChallenge but
 // asks the RP to verify against verifyChallenge, modeling a replayed or
+// mismatched challenge. It returns the RP verification error.
+func authenticateWithChallengeMismatch(rp *RelyingParty, va *VirtualAuthenticator, signChallenge, verifyChallenge Challenge, origin string) error {
+	cred, err := selectCredential(va, rp.ID, nil)
+	if err != nil {
+		return err
+	}
