05f2147716643751ca88f5837d9c2fbe770bc80e
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-07-08 17:41:00 +0700

    docs: document json encoding

--- internal/harbor/helpers.go
+++ internal/harbor/helpers.go
@@ -5,3 +5,9 @@ import (
 	"errors"
 	"time"
 )
+
+// nowNS returns the current monotonic clock reading in nanoseconds.
+func nowNS() int64 { return time.Now().UnixNano() }
+
+// authenticateWithChallengeMismatch signs an assertion over signChallenge but
+// asks the RP to verify against verifyChallenge, modeling a replayed or
