81f45c33fad9fe6c8adc34ffbc0e43f4fa4d3923
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2017-07-21 17:27:00 +0700

    chore: base64url codec

--- internal/harbor/report.go
+++ internal/harbor/report.go
@@ -9,3 +9,13 @@ import (
 	"time"
 )
 
+// ReportSchemaVersion identifies the JSON report shape consumed by the
+// TypeScript browser lab. Bump on breaking changes.
+const ReportSchemaVersion = "passkeypier/report/v1"
+
+// Report is the top-level conformance report emitted after a scenario run.
+type Report struct {
+	Schema      string           `json:"schema"`
+	Tool        string           `json:"tool"`
+	GeneratedAt string           `json:"generated_at"`
+	Summary     ReportSummary    `json:"summary"`
