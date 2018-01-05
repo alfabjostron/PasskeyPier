c9b302589a624c9652fbe39f38b83327f8b327de
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2018-01-05 11:21:00 +0700

    build: sample fixtures

--- web/src/types.ts
+++ web/src/types.ts
@@ -2,3 +2,9 @@
 // (harbor.Report, schema "passkeypier/report/v1"). Keep these in sync with
 // internal/harbor/report.go and scenarios.go.
 
+export type Outcome = "pass" | "fail";
+export type Expectation = "accept" | "reject";
+
+export interface ScenarioResult {
+  name: string;
+  category: string;
