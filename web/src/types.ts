0bec40af3f53cd80e18d428a356d756b217d246e
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2018-01-05 11:03:00 +0700

    test: cover browser lab render

---
+++ web/src/types.ts
@@ -0,0 +1,4 @@
+// Type definitions mirroring the Go conformance report schema
+// (harbor.Report, schema "passkeypier/report/v1"). Keep these in sync with
+// internal/harbor/report.go and scenarios.go.
+
