6aea72702ded2533a9152254394c0bfb208f4d1c
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2018-01-22 11:27:00 +0700

    perf: improve report rendering

---
+++ examples/demo_test.go
@@ -0,0 +1,6 @@
+// Package examples contains runnable, documentation-style examples for the
+// passkeypier lab. These use Go's Example test convention so they compile and
+// their output is verified by `go test`.
+package examples
+
+import (
