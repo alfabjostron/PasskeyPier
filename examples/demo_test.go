c27decd891fd87969748195021648bf453eb4f37
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2018-02-06 15:15:00 +0700

    refactor: tidy browser lab render

--- examples/demo_test.go
+++ examples/demo_test.go
@@ -12,3 +12,11 @@ import (
 // ExampleRegisterAndAuthenticate walks through an honest passkey ceremony:
 // the relying party issues a challenge, the virtual authenticator mints an
 // Ed25519 credential and signs an assertion, and the RP verifies everything.
+func Example_registerAndAuthenticate() {
+	const (
+		rpID   = "harbor.example"
+		origin = "https://harbor.example"
+	)
+
+	rp := harbor.NewRelyingParty(rpID, origin)
+	va := harbor.NewVirtualAuthenticator(true) // UV-capable (biometric/PIN)
