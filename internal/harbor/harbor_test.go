9705eb5186711cb35a784660e2059fbafe48d4a1
23106249+alfabjostron@users.noreply.github.com <23106249+alfabjostron@users.noreply.github.com>
2018-02-15 09:31:00 +0700

    docs: expand attestation formats

--- internal/harbor/harbor_test.go
+++ internal/harbor/harbor_test.go
@@ -25,3 +25,21 @@ func sha256Of(t *testing.T, clientDataJSON []byte) []byte {
 	t.Helper()
 	var cd ClientData
 	if err := json.Unmarshal(clientDataJSON, &cd); err != nil {
+		t.Fatalf("unmarshal client data: %v", err)
+	}
+	h, err := cd.Hash()
+	if err != nil {
+		t.Fatalf("hash: %v", err)
+	}
+	return h
+}
+
+func TestNewChallengeLength(t *testing.T) {
+	ch, err := NewChallenge(DefaultChallengeLen)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(ch) != DefaultChallengeLen {
+		t.Fatalf("challenge length = %d, want %d", len(ch), DefaultChallengeLen)
+	}
+	if _, err := NewChallenge(8); err == nil {
